ebcf7ba31d650f0a50a706813bf01a6180655cc3
Jan Cas <jan.l.cas@outlook.com>
2022-02-16 10:38:20 -0700

    upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
 #include <Arduino.h>
 #include "MotorEncoder.hpp"
 
-MotorEncoder motor_encoder(12, 10, 11, 20, 52, 21, 53);
+MotorEncoder motor_encoder(12, 11, 10, 20, 52, 21, 53);
 int speed = 0;
+// int switchPin = A7;
+
 
 void setup() {
   motor_encoder.set_speed(speed);
   motor_encoder.turn_on();
+  
+  // pinMode(switchPin, INPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
+
+  // int switchVal = analogRead(switchPin);
+  
+  // Serial.println(switchVal);
+
+  // if (switchVal == 1023){
+  //   motor_encoder.turn_on();
+  // }else {
+  //   motor_encoder.turn_off();
+  // }
+
   if (Serial.available() > 0){
     int incoming = Serial.read();
     Serial.println(incoming);
@@ -35,8 +50,10 @@ void loop() {
     }
   }
 
+  /*
   Serial.print("encoder A: ");
   Serial.print(motor_encoder.encoder_a_count());
   Serial.print(" || encoder B: ");
   Serial.println(motor_encoder.encoder_b_count());
+  */
 }
