b2495a82fcfd4bb40fdad414dd944286309d89e2
mturja19 <99511985+mturja19@users.noreply.github.com>
2022-02-25 15:31:24 -0700

    FUCKING WORKING VALUES

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 cppQueue queue(sizeof(mm_attr), 2, FIFO, true);
 MotorEncoder motor_encoder(12, 11, 10, 20, 21, 380, 12);
-PID_controller pid(10, .04, 2.0, 1.0, 1.0, .075);
+PID_controller pid(0.368, .028, 0.0046, 0.7833, 1.0, .075);
 
 bool cw = true;
 
@@ -23,12 +23,16 @@ void loop()
 {
 
     double pos = motor_encoder.get_pos();
-    double new_vel = pid.compute(pos, 360, 30);
+    double new_vel = pid.compute(pos, 72, 30);
     // Serial.print("pos: ");
     // Serial.print(pos);
     // Serial.print(" || ");
     // Serial.println(new_vel);
+    if (abs(new_vel) < 10) {
+        motor_encoder.turn_off();
+    } else {
     motor_encoder.set_speed((int) new_vel);
+    }
 
     Serial.println(pos);
 }
