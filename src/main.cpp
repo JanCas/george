f78637587c291b3256955b3b393eef712f563b1f
Jan Cas <jan.l.cas@outlook.com>
2022-03-22 13:53:34 -0600

    no PID class

--- src/main.cpp
+++ src/main.cpp
@@ -15,36 +15,13 @@
 bool running = false;
 
 PID_controller pid(4, .7, 3.15, .075);
-MotorEncoder motor_encoder(12, 11, 10, 20, 21, 380, 12, &pid);
+MotorEncoder motor_encoder(12, 11, 10, 20, 21, 380, 12);
 
 double pos_des = 72.5;
 
 void setup()
 {
     Serial.begin(9600);
-    motor_encoder.set_speed(75);
-/*
-    Serial.write('a'); // this is to notify MATLAB that Arduino is ready to receive parameters
-    String inString = "";
-    while (Serial.available() == 0);
-    inString = Serial.readStringUntil(' ');
-    double Kp = inString.toFloat();
-    inString = Serial.readStringUntil(' ');
-    double Kd = inString.toFloat();
-    inString = Serial.readStringUntil(' ');
-    double Ki = inString.toFloat();
-    inString = Serial.readStringUntil(' ');
-    double StepSize = inString.toFloat();
-    inString = Serial.readStringUntil(' ');
-    double alpha = inString.toFloat();
-    inString = Serial.readStringUntil(' ');
-    double StepTime = inString.toFloat();
-
-    pos_des = StepSize;
-    pid.set_config_values(Kp, Kd, Ki, alpha, StepTime);
-    double t0 = micros() / 1000000.;
-    */
-   motor_encoder.set_speed_constrain(255);
 }
 
 void loop()
