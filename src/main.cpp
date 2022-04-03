d225f1c7e1217e6a4fdedfc18477596566331f01
Jan Cas <jan.l.cas@outlook.com>
2022-04-03 12:17:20 -0600

    main

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,20 @@
 
 #include "ConfigParser.hpp"
 
-ConfigParser config((const int[]) {1,2}, (const int[]) {3,4,5}, (const int[]) {6,7});
+ConfigParser config((const int[]) {41,43}, (const int[]) {45,47,49}, (const int[]) {51,55});
 
 void setup(){
-    config.init();
+    // config.init();
+    Serial.begin(9600);
+    pinMode(5, INPUT);
+    pinMode(A0, INPUT);
 }
 
 void loop() {
-    if (config.read(1)){
-        Serial.print(config.get_color());
-        Serial.print(" || ");
-        Serial.println(config.get_queue_size());
-    }
+    Serial.println(analogRead(A0));
+    // if (config.read(1)){
+    //     Serial.print(config.get_color());
+    //     Serial.print(" || ");
+    //     Serial.println(config.get_queue_size());
+    // }
 }
