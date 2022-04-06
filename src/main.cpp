93ef7c12a6a2606d09e5968567c57b0a76fb0cf3
Jan Cas <jan.l.cas@outlook.com>
2022-04-06 13:20:44 -0600

    up

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int downstream_pin = 8;
 int button_pin = 13;
 int led_pin = 6;
 
-ConfigParser config((const int[]) {41,43}, (const int[]) {45,47,49}, (const int[]) {51,55});
+// ConfigParser config((const int[]) {41,43}, (const int[]) {45,47,49}, (const int[]) {51,55});
 
 MotorEncoder motor_encoder(12,11,10,2,3,380,12);
 
@@ -20,28 +20,10 @@ void setup(){
     pinMode(13, INPUT);
     pinMode(8, INPUT);
     pinMode(6, OUTPUT);
-    motor_encoder.set_speed(45);
+    // motor_encoder.set_speed(45);
+    // motor_encoder.set_pid_values
 }
 
 void loop() {
-    int hand_value = analogRead(A0);
 
-    if (hand_value > 250){
-        running = false;
-    }
-    if (digitalRead(downstream_pin) == HIGH){
-       running = false;
-       Serial.println("HERE");
-    }
-    if (!running){
-        motor_encoder.turn_off();
-        Serial.println("sending other modules to stop");
-        if (digitalRead(button_pin) == HIGH){
-            running = true;
-            digitalWrite(led_pin, LOW);
-        }
-    }
-    else{
-        motor_encoder.turn_on();
-    }
 }
